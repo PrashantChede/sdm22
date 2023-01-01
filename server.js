68801f8d307022fe1a5a4b87b62b9c5931ee19b2
kavita <sehgalkavita27@gmail.com>
2023-01-01 20:42:39 +0530

    done1

---
+++ server.js
@@ -0,0 +1,11 @@
+var express=require("express");
+const app=express();
+var bodyparser=require("body-parser");
+var path=require("path");
+const e = require("express");
+
+app.get("/",function(req,resp){
+    resp.sendFile(path.join(__dirname,"./public/index.html"));
+});
+app.listen(7777);
+console.log("server is listening on port 7777");
